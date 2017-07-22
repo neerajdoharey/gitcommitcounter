cf520ba92d8bc5fbd949e8c8974febcf1e6afde9
Neeraj Doharey <neerajlivewire@gmail.com>
2017-07-22 10:55:11 +0530

    Changes rails 3.2 to 42.8 , Adds change branch feature, active link in navigation

--- app/controllers/git_commit_controller.rb
+++ app/controllers/git_commit_controller.rb
@@ -7,19 +7,25 @@ class GitCommitController < ApplicationController
 
   def create
       if !params[:dir_name].blank?
-         @logs,@dirname =repo_commit(params[:dir_name])
+         @logs,@dirname,@branch = repo_commit(params[:dir_name])
       else
          @myurl = params[:user][:myurl]
          # @logarray = file_path(@myurl)
-         logarray,@dirname = clone_repo(@myurl)
-         #WillPaginate::Collection.create(1, 10, logarray.length)
-         @logs = logarray#.paginate(:page => params[:page], :per_page => 30)
-
+         logarray,@dirname,@branch = clone_repo(@myurl)
+         @logs = logarray#.paginate(:page => params[:page], :per_page => 5)
       end
-
    end
 
    def repository
       @list = directory_list
+   end
+
+   def list_branches
+     @branch_name = list_branch(params[:dir_name])
+   end
+
+   def change_branch
+     change_repo_branch(params[:dir_name], params[:branch_name])
+     redirect_to :action=>"create",:dir_name=>params[:dir_name]
    end
 end
--- lib/file_handling.rb
+++ lib/file_handling.rb
@@ -1,15 +1,14 @@
 module FileHandling
-	
+	def change_directory(dir_name="")
+		cpath =  File.join(File.dirname(__FILE__),'..','tmp','gitrepo',dir_name)
+		change_path=Dir.chdir(cpath)
+	end
 	def clone_repo(myurl)
-
-
 	  	urlarray=@myurl.split("/")
 	  	repo_dir=urlarray[-1].split(".")
 
-	  	cpath =  File.join(File.dirname(__FILE__),'..','gitrepo')
-	  	change_path=Dir.chdir(cpath)
+	  	change_directory
 	  	`git clone #{@myurl}`
-
 
 	  	#logarray=repo_commit(repo_dir[0].to_s)
 	  	Dir.chdir(repo_dir[0].to_s)
@@ -22,46 +21,59 @@ module FileHandling
 	end
 
 	def directory_list
-		cpath =  File.join(File.dirname(__FILE__),'..','gitrepo')
-		change_path=Dir.chdir(cpath)
+		change_directory
 		Dir.entries('.')
-
-
 	end
 
 	def repo_commit(dir_name)
-		cpath =  File.join(File.dirname(__FILE__),'..','gitrepo',"#{dir_name}")
-		change_path=Dir.chdir(cpath)
+		# cpath =  File.join(File.dirname(__FILE__),'..','tmp','gitrepo',"#{dir_name}")
+		# change_path=Dir.chdir(cpath)
+		change_directory(dir_name)
 		log = `git log`
-	  	logarray = log.split("commit")
-  		logarray.delete_at(0)
-
-  		return hash_convert(logarray) , dir_name
+		branch = `git branch`
+		special = "*"
+		regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+		curret_branch = branch.split("\n").select{ |i| i if i =~ regex }
+  	logarray = log.split("commit")
+		logarray.delete_at(0)
+		return hash_convert(logarray) , dir_name, curret_branch[0]
 
 	end
 
 	def hash_convert(logarray)
-
 		log_hash={}
 		log_hash_array=[]
 		logarray.each_with_index do |arr,id|
-			commit=arr.split("Author:") 
+			commit=arr.split("Author:")
 			author=commit[1].to_s.split("Date:")
-			dat=author[1].to_s.split("\n\n") 
+			dat=author[1].to_s.split("\n\n")
 			comment= dat[1].to_s.split("\n")
-			
-				log_hash["id"]=id+1 
-				log_hash["commit"]=commit[0] 
-				log_hash["author"]=author[0] 
-				log_hash["time"]=dat[0]#dat.nil? ? dat[0] : Date.parse(dat[0])
-				
-				log_hash["comment"]=comment.join("|")	
-				log_hash_array<<log_hash.clone
-		end 
 
+			log_hash["id"]=id+1
+			log_hash["commit"]=commit[0]
+			log_hash["author"]=author[0]
+			log_hash["time"]=dat[0]#dat.nil? ? dat[0] : Date.parse(dat[0])
+			log_hash["comment"]=comment.join("|")
+			log_hash_array<<log_hash.clone
+		end
 		return log_hash_array
-		
 	end
-	
-	
+
+	def list_branch(branch_dir)
+		# cpath =  File.join(File.dirname(__FILE__),'..','tmp','gitrepo',branch_dir)
+		# change_path=Dir.chdir(cpath)
+		change_directory(branch_dir)
+		branches = `git branch -r`
+		branch_names =  branches.split("\n")
+		puts branch_names.inspect
+		return branch_names
+	end
+
+	def change_repo_branch(branch_dir ,branch_name)
+		# cpath =  File.join(File.dirname(__FILE__),'..','tmp','gitrepo',branch_dir)
+		# change_path=Dir.chdir(cpath)
+		change_directory(branch_dir)
+		puts `git branch`
+		puts `git checkout #{branch_name}`
+	end
 end
